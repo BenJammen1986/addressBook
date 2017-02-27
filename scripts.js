9f685772c69781882c722296014001750a26c98f
Grace Stuart <gstuart@gmail.com>
2017-02-27 07:21:25 -0800

    add front end logic of doc.ready and form.submit.

--- scripts.js
+++ scripts.js
@@ -3,3 +3,10 @@ function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
+
+//front-end logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+
+  });
+});
