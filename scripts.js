c70f27b51ea3ca5b98bacb159bb3df4d93c085d8
Grace Stuart and Megan Warnock <megan@>
2017-02-27 08:20:03 -0800

    add method for display results in li.

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
   });
