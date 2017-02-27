8bd6d704cc2c6ba647466c1ec035d0142bfa458f
Grace Stuart and Megan Warnock <megan@>
2017-02-27 09:39:38 -0800

    remove unused code.

--- scripts.js
+++ scripts.js
@@ -16,12 +16,10 @@ debugger;
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() +  "</span></li>");
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContat.firstName);
+      $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
     });
   });
