6937fb2742c9240dca888a3093a6798bec99398c
Grace Stuart and Megan Warnock <megan@>
2017-02-27 08:08:30 -0800

    add front end logic.

--- scripts.js
+++ scripts.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
   });
 });
