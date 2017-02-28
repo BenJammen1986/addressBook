afa83c7bb9a510153c198dfbff9819caa9709de1
Grace Stuart and Megan Warnock <megan@>
2017-02-27 17:19:27 -0800

    complete code.

--- scripts.js
+++ scripts.js
@@ -11,29 +11,62 @@ function Address(street, city, state) {
   this.state = state;
 }
 
- Contact.prototype.fullName = function() {
+Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-//newContact.addresses.push();
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
 
 //front-end logic
+// user interface logic
 $(document).ready(function() {
+
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
-debugger;
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() +  "</span></li>");
-    //this clears the input field so new data can be entered. This is solely to make the UI more user friendly.
-      $("input#new-first-name").val("");
-      $("input#new-last-name").val("");
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
+    //following code NEEDS to be located after creating a new contact.
+      $(".new-address").each(function() {
+        var inputtedStreet = $(this).find("input.new-street").val();
+        var inputtedCity = $(this).find("input.new-city").val();
+        var inputtedState = $(this).find("input.new-state").val();
+        var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+        newContact.addresses.push(newAddress)
+      });
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+  $(".contact").last().click(function() {
+    $("#show-contact").show();
+    $("#show-contact h2").text(newContact.fullName());
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
+    $("ul#addresses").text("");
+    newContact.addresses.forEach(function(address) {
+      $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
     });
+  });
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
   });
 });
